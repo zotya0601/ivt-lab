a09c80b765c9ddbeeef0156126b5ad1620c04a04
zotya0601 <zotya0601@gmail.com>
2023-04-21 13:23:23 +0200

    merge conflict from branch-B

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -1,7 +1,7 @@
 package hu.bme.mit.spaceship;
 
 /**
-* A simple spaceship with two proton torpedo stores and four lasers
+* A simple spaceship with two proton torpedo stores and four legs
 */
 public class GT4500 implements SpaceShip {
 
